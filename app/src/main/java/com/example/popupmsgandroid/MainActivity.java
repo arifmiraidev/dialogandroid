d7286aa4b232e05dfabf4c4e5b8d4a81e680f621
Lenovo <sssyedarif98@gmail.com>
2022-11-30 16:52:40 +0800

    changed from dialog popup to displaying new activity

--- app/src/main/java/com/example/popupmsgandroid/MainActivity.java
+++ app/src/main/java/com/example/popupmsgandroid/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 //main function
 public class MainActivity extends AppCompatActivity {
@@ -25,19 +28,34 @@ public class MainActivity extends AppCompatActivity {
 
 //        assign value and creating object
         popupButton= findViewById(R.id.button);
-        popupDialog= new Dialog(this);
+        //popupDialog= new Dialog(this);
 
 //        function called when button is clicked and display new layout
         popupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                popupDialog.setContentView(R.layout.popupwindow);
-                popupDialog.getWindow().setBackgroundDrawable(new ColorDrawable());
-                popupDialog.show();
+//                popupDialog.setContentView(R.layout.popupwindow);
+//                popupDialog.getWindow().setBackgroundDrawable(new ColorDrawable());
+//                popupDialog.show();
+
+                Intent intent = new Intent(MainActivity.this,dialogpopup.class);
+                startActivity(intent);
+
+
+
+//                maketoast();
+
+
 
             }
         });
 
+
+    }
+
+    void maketoast(){
+
+        Toast.makeText(getApplicationContext(),"Testing toast",Toast.LENGTH_SHORT).show();
 
     }
 }
---
+++ app/src/main/java/com/example/popupmsgandroid/dialogpopup.java
@@ -0,0 +1,14 @@
+package com.example.popupmsgandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dialogpopup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialogpopup);
+    }
+}
